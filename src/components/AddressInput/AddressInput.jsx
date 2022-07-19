8fa20595a758113890c5b348f77824c7cb4e725e
anton278 <nakonechnyi.an@gmail.com>
2022-07-19 18:01:17 +0300

    Improved Search
    If the string is empty, no requests are sent.

--- src/components/AddressInput/AddressInput.jsx
+++ src/components/AddressInput/AddressInput.jsx
@@ -1,9 +1,18 @@
 import { StyledInput, StyledErrorP } from "../../assets/styledComponents";
 import { getAutosuggests } from "../../redux/asyncActions";
 import { useDispatch } from "react-redux";
+import { SET_RESULTS } from "../../redux/reducersAndActions";
 
-const AddressInput = ({ label, register, errors }, ref) => {
+const AddressInput = ({ label, register, errors, watch }) => {
     const dispatch = useDispatch();
+
+    const onChangeHandler = (e) => {
+        if (e.target.value.trim().length) {
+            dispatch(getAutosuggests(watch(e.target.value)));
+        } else {
+            dispatch({ type: SET_RESULTS, payload: [] });
+        }
+    };
 
     return (
         <>
@@ -12,7 +21,7 @@ const AddressInput = ({ label, register, errors }, ref) => {
                 {...register(label, { required: true })}
                 placeholder="Address"
                 error={errors[label] ? true : false}
-                onChange={(e) => dispatch(getAutosuggests(e.target.value))}
+                onChange={onChangeHandler}
             ></StyledInput>
             {errors[label] && (
                 <StyledErrorP>This field is required</StyledErrorP>
