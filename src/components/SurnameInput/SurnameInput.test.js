0baf487b27e27809fffe4cc875df948a3e46689f
anton278 <nakonechnyi.an@gmail.com>
2022-07-20 16:50:01 +0300

    Added test

---
+++ src/components/SurnameInput/SurnameInput.test.js
@@ -0,0 +1,30 @@
+import { shallow } from "enzyme";
+import React from "react";
+import SurnameInput from "./SurnameInput";
+import Adapter from "enzyme-adapter-react-16";
+import Enzyme from "enzyme";
+import { Row, StyledInput, StyledErrorP } from "../../assets/styledComponents";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("component should render", () => {
+    test("row and input in it", () => {
+        const component = shallow(
+            <SurnameInput label="name" register={() => {}} errors={{}} />
+        );
+        expect(component.find(Row)).toHaveLength(1);
+        expect(component.find(Row).find(StyledInput)).toHaveLength(1);
+    });
+    test("row, input, error", () => {
+        const component = shallow(
+            <SurnameInput
+                label="name"
+                register={() => {}}
+                errors={{ name: {} }}
+            />
+        );
+        expect(component.find(Row)).toHaveLength(1);
+        expect(component.find(Row).find(StyledInput)).toHaveLength(1);
+        expect(component.find(StyledErrorP)).toHaveLength(1);
+    });
+});
